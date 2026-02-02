d3055f2e75c3ce17e97db89f08514c6fb5c9b703
Tontonchikc <pranganton@gmail.com>
2026-02-02 16:37:06 +0200

    string

---
+++ index.js
@@ -0,0 +1,19 @@
+const result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+const email = "prangantonn@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+let fullName = word1 + " " + word2 + " " + word3;
+fullName += " Viktor";
+console.log(fullName);
+
+const userName = prompt("Ваше ім'я");
+const payment = prompt("Сума донату");
+alert(`Дякуємо, ${userName}! 3a донат y розмірі ${payment} гривень`);
+
